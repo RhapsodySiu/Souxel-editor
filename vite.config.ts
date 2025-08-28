ea3ca312e5b7376d72771fec542737da22f3b1cb
Yve Siu <34149034+RhapsodySiu@users.noreply.github.com>
2025-08-28 11:41:24 +0800

    fix git action

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/Souxel-editor/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
     mode === 'development' &&
